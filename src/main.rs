8938b2d9fb522f8958b6eb967e01cd9f76258c14
Justin Johnson <justinthomasjohnson@gmail.com>
2020-01-21 15:53:00 -0800

    Add basic client code (#2)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::net::{TcpStream};
+use std::io::{Read, Write};
+use std::str::from_utf8;
+
+const RESPONSE_SIZE: usize = 6;
+
+fn get_task_addr() -> String {
+    String::from("54.183.196.119:3333")
+}
+
 fn main() {
-    println!("Hello, world!");
+    match TcpStream::connect(get_task_addr()) {
+        Ok(mut stream) => {
+            let msg = b"hello!";
+            stream.write(msg).unwrap();
+
+            let mut data = [0 as u8; RESPONSE_SIZE];
+            match stream.read_exact(&mut data) {
+                Ok(_) => {
+                    if &data == msg {
+                        println!("Reply is ok!")
+                    } else {
+                        let text = from_utf8(&data).unwrap();
+                        println!("unexpected reply: {}", text);
+                    }
+                },
+                Err(e) => {
+                    println!("{}", e);
+                },
+            }
+        },
+        Err(e) => {
+            // TODO(ljoswiak): Handle/log error.
+            println!("{}", e);
+        },
+    }
 }
