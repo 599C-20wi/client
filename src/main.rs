c3781b0bb50ba2626f36d4c792881c111d9257f0
Lukas Joswiak <lukas@lukasjoswiak.com>
2020-02-12 14:35:54 -0800

    Pull task servers from assigner (#10)

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate log;
 
+use std::collections::HashMap;
 use std::env;
 use std::error;
 use std::fmt;
@@ -14,15 +15,14 @@ use rand::distributions::Uniform;
 use rand::seq::IteratorRandom;
 use rand::{thread_rng, Rng};
 
+use assigner::hash;
+use assigner::message::{Assignment, Get};
 use task::face::Expression;
 use task::message::{Request, Response};
 
 const BUFFER_SIZE: usize = 256;
 
-fn get_task_addr() -> String {
-    // String::from("54.183.196.119:3333")
-    String::from("localhost:3333")
-}
+const ASSIGNER_ADDRESS: &str = "184.169.220.191:4333";
 
 // Reads the given file into buffer.
 fn read_file(filename: &std::path::PathBuf, buffer: &mut Vec<u8>) -> Result<usize, std::io::Error> {
@@ -56,9 +56,16 @@ fn generate_expression(
 fn main() {
     simple_logger::init().unwrap();
 
+    // Map of expressions -> vector of task servers to handle requests.
+    let mut assignments: HashMap<Expression, Vec<String>> = HashMap::new();
+    let mut server_index = 0;
+
+    // Cache open TCP streams.
+    let mut streams: HashMap<String, TcpStream> = HashMap::new();
+
     let args: Vec<String> = env::args().collect();
     if args.len() != 2 {
-        println!("usage: run cargo <number-of-requests>");
+        println!("usage: cargo run <number-of-requests>");
         process::exit(1);
     }
     let repeat: i32 = args[1].parse().unwrap();
@@ -76,67 +83,131 @@ fn main() {
     let mut rng = thread_rng();
     let distribution = Uniform::new(0, 2);
 
-    match TcpStream::connect(get_task_addr()) {
-        Ok(mut stream) => {
-            for _ in 0..repeat {
-                let expression = match generate_expression(&mut rng, &distribution) {
-                    Ok(expression) => expression,
-                    Err(error) => {
-                        error!("failed to generate expression: {}", error);
-                        continue;
-                    }
-                };
+    'send: for _ in 0..repeat {
+        let expression = match generate_expression(&mut rng, &distribution) {
+            Ok(expression) => expression,
+            Err(error) => {
+                error!("failed to generate expression: {}", error);
+                continue;
+            }
+        };
 
-                // Randomly select an image and read it into memory.
-                let image_path = entries.iter().choose(&mut rng).unwrap();
-                let mut image_buffer = Vec::new();
-                read_file(image_path, &mut image_buffer).expect("failed to read file");
+        // Randomly select an image and read it into memory.
+        let image_path = entries.iter().choose(&mut rng).unwrap();
+        let mut image_buffer = Vec::new();
+        read_file(image_path, &mut image_buffer).expect("failed to read file");
 
-                debug!(
-                    "sending image: {:?} with expression {:?}",
-                    image_path, expression
-                );
-                let message = Request {
-                    expression,
-                    image: image_buffer,
-                };
-                let serialized = message.serialize();
-                stream.write_all(serialized.as_bytes()).unwrap();
+        // Figure out which task server to send request to by looking in cache
+        // or asking the assigner if value is not cached.
+        if assignments.get(&expression).is_none() {
+            debug!("querying assigner for expression {:?}", expression);
+            // Send request to assigner.
+            let assignment = match TcpStream::connect(ASSIGNER_ADDRESS) {
+                Ok(mut stream) => {
+                    let get = Get {
+                        slice_key: hash::to_slice_key(&expression),
+                    };
+                    let serialized = get.serialize();
+                    stream.write_all(serialized.as_bytes()).unwrap();
 
-                let mut buffer = [0 as u8; BUFFER_SIZE];
-                match stream.read(&mut buffer) {
-                    Ok(size) => {
-                        if size == 0 {
-                            // Server died.
-                            return;
+                    let mut buffer = [0 as u8; BUFFER_SIZE];
+                    match stream.read(&mut buffer) {
+                        Ok(size) => {
+                            if size == 0 {
+                                // Assigner died.
+                                return;
+                            }
+
+                            match Assignment::deserialize(&buffer[..size]) {
+                                Ok(message) => message,
+                                Err(e) => {
+                                    error!("deserialization failed: {}", e);
+                                    std::process::exit(1);
+                                }
+                            }
                         }
-
-                        let response = match Response::deserialize(&buffer[..size]) {
-                            Ok(message) => message,
-                            Err(e) => {
-                                error!("deserialization failed: {}", e);
-                                std::process::exit(1);
-                            }
-                        };
-
-                        match response {
-                            Response::Accept { matches_expression } => {
-                                info!("received accept response: {:?}", matches_expression);
-                            }
-                            Response::Reject { error } => {
-                                info!("received reject response: {}", error);
-                            }
-                        };
-                    }
-                    Err(e) => {
-                        println!("{}", e);
-                        error!("stream read failed: {}", e);
+                        Err(e) => {
+                            error!("stream read failed: {}", e);
+                            continue 'send;
+                        }
                     }
                 }
-            }
+                Err(e) => {
+                    error!("failed to connect to assigner: {}", e);
+                    continue 'send;
+                }
+            };
+            info!(
+                "received assignment for {:?}: {:?}",
+                expression, assignment.addresses
+            );
+            assignments.insert(expression.clone(), assignment.addresses);
+            server_index = 0;
         }
-        Err(e) => {
-            error!("failed to connect to server: {}", e);
+
+        let tasks = assignments.get(&expression).unwrap();
+        let task = &tasks[server_index];
+        server_index += 1;
+
+        if streams.get(task).is_none() {
+            // TCP stream not cached, open new connection to task server.
+            let stream = match TcpStream::connect(task) {
+                Ok(stream) => stream,
+                Err(e) => {
+                    error!("failed to connect to task server {}: {}", task, e);
+                    continue 'send;
+                }
+            };
+            streams.insert(task.to_string(), stream);
+        }
+
+        debug!(
+            "sending image: {:?} with expression {:?} to {}",
+            image_path, expression, task
+        );
+        let mut stream = streams.get(task).unwrap();
+        let message = Request {
+            expression,
+            image: image_buffer,
+        };
+        let serialized = message.serialize();
+        stream.write_all(serialized.as_bytes()).unwrap();
+
+        let mut buffer = [0 as u8; BUFFER_SIZE];
+        match stream.read(&mut buffer) {
+            Ok(size) => {
+                if size == 0 {
+                    // Server died.
+                    return;
+                }
+
+                let response = match Response::deserialize(&buffer[..size]) {
+                    Ok(message) => message,
+                    Err(e) => {
+                        error!("deserialization failed: {}", e);
+                        std::process::exit(1);
+                    }
+                };
+
+                match response {
+                    Response::Accept { matches_expression } => {
+                        info!("received accept response: {:?}", matches_expression);
+                    }
+                    Response::Reject {
+                        error_msg: error,
+                        expression,
+                    } => {
+                        info!(
+                            "received reject response: {} for expression {:?}",
+                            error, expression
+                        );
+                    }
+                };
+            }
+            Err(e) => {
+                error!("stream read failed: {}", e);
+                continue 'send;
+            }
         }
     }
 }
